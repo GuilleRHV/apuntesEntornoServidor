8191de7b9a970c5ce6b99ae54d66c7983c8cc566
guillermo rodriguez <guillerhuguet@gmail.com>
2022-09-28 17:11:10 +0100

    segundo commit

---
+++ data/ejercicios/ej1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>ESTO ES UNA PÁGINA</h3>
+    <?php
+    echo 'estoy en php (echo)<br>';
+    print 'estoy en php(print)<br>';
+    
+    ?>
+    <?="<h3>otra forma de hacer echo</h3>";?>
+</body>
+</html>
---
+++ data/ejercicios/ej2.php
@@ -0,0 +1,25 @@
+<?php
+$var1 = 5;
+$var2 = "7";
+$var2 =(int)"7";
+
+$var3 = (string)8;
+//Concatenar se usa el punto: .
+echo "var 1 es del tipo " . gettype($var3);
+/* 
+    variables: [A-Z,a-z,0-9]
+        - No pueden comenzar con numeros ni caraceres especiales
+        
+        
+    -----------------------------------
+        TIPOS DE DATOS
+            int,string,boolean,float
+            arrays, objetos, ficheros
+        tipado de lenguaje de programacion
+            -Fuerte: En Java es fuerte (int var1 = 0)
+            -Debil: PHP ($var1 = 0)
+        
+        typ VSC ctrl+mayus+7 para comentar lineas seleccionadas
+        getType();
+        */
+
